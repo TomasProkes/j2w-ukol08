6d7d33a3d02925c8eba4e203385acf59c0e8e285
TomasProkes <tomas.prokes@centrum.cz>
2021-05-30 00:05:13 +0200

    Add the post repository

--- src/main/java/cz/czechitas/java2webapps/ukol8/repository/PostRepository.java
+++ src/main/java/cz/czechitas/java2webapps/ukol8/repository/PostRepository.java
@@ -1,4 +1,15 @@
 package cz.czechitas.java2webapps.ukol8.repository;
 
-public class PostRepository {
+import cz.czechitas.java2webapps.ukol8.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Repository for table {@code Post}
+ */
+@Repository
+public interface PostRepository extends JpaRepository<Post, Integer> {
+    /**
+     * Search methods will be added here
+     */
 }
