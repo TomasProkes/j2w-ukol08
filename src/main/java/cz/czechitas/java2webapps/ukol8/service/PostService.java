2e120b5741a722d8a1087cc91df26cc2c5e17555
TomasProkes <tomas.prokes@centrum.cz>
2021-05-30 00:18:32 +0200

    Add a list method to the post service

--- src/main/java/cz/czechitas/java2webapps/ukol8/service/PostService.java
+++ src/main/java/cz/czechitas/java2webapps/ukol8/service/PostService.java
@@ -1,7 +1,10 @@
 package cz.czechitas.java2webapps.ukol8.service;
 
+import cz.czechitas.java2webapps.ukol8.entity.Post;
 import cz.czechitas.java2webapps.ukol8.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 /**
@@ -14,5 +17,12 @@ public class PostService {
     @Autowired
     public PostService(PostRepository postRepository) {
         this.postRepository = postRepository;
+    }
+
+    /**
+     * Returns a pageable list of all posts in the database (though not sorted yet)
+     */
+    public Page<Post> list(Pageable pageable) {
+        return postRepository.findAll(pageable);
     }
 }
