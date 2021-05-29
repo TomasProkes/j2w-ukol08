19fed682e6b17f7fea09c19b5af57caf9c9e85d6
TomasProkes <tomas.prokes@centrum.cz>
2021-05-30 00:13:50 +0200

    Add the post service

--- src/main/java/cz/czechitas/java2webapps/ukol8/service/PostService.java
+++ src/main/java/cz/czechitas/java2webapps/ukol8/service/PostService.java
@@ -1,4 +1,18 @@
 package cz.czechitas.java2webapps.ukol8.service;
 
+import cz.czechitas.java2webapps.ukol8.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Service for handling blog posts
+ */
+@Service
 public class PostService {
+    private final PostRepository postRepository;
+
+    @Autowired
+    public PostService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
 }
