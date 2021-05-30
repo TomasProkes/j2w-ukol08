a171f8bcafe031546093937c01da8daf3c98f580
TomasProkes <tomas.prokes@centrum.cz>
2021-05-30 23:09:31 +0200

    Add test records to troubleshoot future post display issue

---
+++ src/main/resources/db/migration/V2__data.sql
@@ -0,0 +1,71 @@
+INSERT INTO post
+(
+    slug,
+    author,
+    title,
+    perex,
+    body,
+    published
+) VALUES
+(
+    'first-post-this-year',
+    'Timothy Snyder',
+    'Donald Trump borrows from the old tricks of fascism',
+    '<header><p>The idea that the powerful are victims who must be coddled arose in a setting that recalls the United States of today</p></header>',
+    '<section><p>' ||
+    'The governing principle of the Trump administration is total irresponsibility, a claim of innocence from a position of power, something which happens to be an old fascist trick. As we see in the president’s reactions to American rightwing terrorism, he will always claim victimhood for himself and shift blame to the actual victims. As we see in the motivations of the terrorists themselves, and in the long history of fascism, this maneuver can lead to murder.' ||
+    '</p><p>' ||
+    'The Nazis claimed a monopoly on victimhood. Mein Kampf includes a lengthy pout about how Jews and other non-Germans made Hitler’s life as a young man in the Habsburg monarchy difficult. After stormtroopers attacked others in Germany in the early 1930s, they made a great fuss if one of their own was injured. The Horst Wessel Song, recalling a single Nazi who was killed, was on the lips of Germans who killed millions of people. The second world war was for the Nazis’ self-defense against “global Jewry”.' ||
+    '</p><p>' ||
+    'The idea that the powerful must be coddled arose in a setting that recalls the United States of today. The Habsburg monarchy of Hitler’s youth was a multinational country with democratic institutions and a free press. Some Germans, members of the dominant nationality, felt threatened because others could vote and publish. Hitler was an extreme example of this kind of sentiment. Today, some white Americans are similarly threatened by the presence of others in institutions they think of as their own. Among the targets of the accused pipe bomber were four women, five black people and two Jews. Just as (some) Germans were the only serious national problem within the Habsburg monarchy, so today are (some) white Americans the only serious threat to their own republic.' ||
+    '</p><p>' ||
+    'Hitler formulated his version of total irresponsibility after the disaster of the first world war, which destroyed the Habsburg monarchy and fragmented its German ally. He found an explanation for the disaster that spared the ego of the German nationalists who had supported it. The world was a struggle, Hitler maintained, among superior and inferior races. If superior Germans were somehow defeated in a war, this only proved that an invisible power stood behind the visible facts: global Jewry.' ||
+    '</p><p>' ||
+    'According to Hitler, Jews inculcated ideas, such as that of individual rights, that drew people away from their natural bloodlust. The notion that Jews are responsible for civil rights or immigrant protection, one that seems to have motivated the mass shooting at the Tree of Life synagogue in Pittsburgh, is an example of this Hitlerian way of thinking. Since Jews are supposedly responsible for rights, they are blamed when people beyond the dominant group exercise rights. Because the spread of the norm of rights takes place in the mind, the only response, thought Hitler, was to remove Jews from the planet. The accused Pittsburgh murderer (“all Jews must die”) seems to have thought in just this way.' ||
+    '</p><p>' ||
+    'The attraction of the Nazi conspiracy thinking is that we can feel like victims when we attack. Its vulnerability is that the world is full of facts. Hence Hitler’s hostility to journalism. In the Germany of the early 1930s, the newspaper industry was suffering after a financial crisis. Hitler and other Nazis used the idea of the “Lügenpresse” (“fake news”) to attack remaining journalists who were trying to report the facts. In Germany and Austria today, the far right once more speaks of the Lügenpresse, in part because the American president has made the idea respectable. The extreme right in Germany and Austria knows perfectly well that “fake news” is American English for Lügenpresse.' ||
+    '</p><p>' ||
+    'In the United States today, reporting was already in trouble for similar reasons before Trump, like Hitler, began to claim that the reporters who seek the facts are liars and enemies. Naturally, the president denies responsibility when people take him at his word and draw instead from the conspiracy thinking he himself spreads. Trump blames the press for attempts to murder members of the press. He seizes the occasion, as always, to present himself as the true victim. The facts hurt his feelings.' ||
+    '</p><p>' ||
+    'Trump and some of his supporters mount a strategy of deterrence by narcissism: if you note our debts to fascism, we will up the pitch of the whining. Thus Trump can base his rhetoric on the fascist idea of us and them, lead fascist chants at rallies, encourage his supporters to use violence, praise a politician who attacked a journalist, muse that Hillary Clinton should be assassinated, denigrate the intelligence of African Americans, associate migrants with criminality, run an antisemitic advertisement, spread the Nazi trope of Jews as “globalists”, and endorse the antisemitic idea that the Jewish financier George Soros is responsible for political opposition – but he and his followers will puff chests and swell sinuses if anyone points this out.' ||
+    '</p><p>' ||
+    'If Trump is not a fascist, this is only in the precise sense that he is not even a fascist. He strikes a fascist pose, and then issues generic palliative remarks and denies responsibility for his words and actions. But since total irresponsibility is a central part of the fascist tradition, it is perhaps best to give Trump his due credit as an innovator.' ||
+    '</p></section>' ||
+    '<footer>' ||
+    'Timothy Snyder is the Levin Professor of History at Yale University. He is the author of several books of European history as well as, most recently, <i>On Tyranny</i> and <i>The Road to Unfreedom</i>' ||
+    '</footer>',
+    '2021-05-30'
+),
+(
+    'confederate-monuments',
+    'Timothy Snyder',
+    'We don''t owe anything to Confederate monuments',
+    '<header><p>Trump spends so much time defending statues not because he cares about history, but precisely because he doesn’t</p></header>',
+    '<section><p>' ||
+    'If you approach me to ask me a question, and I hit you with a baton and spray teargas in your face, I have committed a crime. If you are walking down the street and I forcibly pull you into a van, I have committed a crime. These are now the routine actions of employees of the federal government directed against American citizens. “Rapid deployment teams” of federal employees, now in Portland and soon in other American cities, are committing crimes on the basis of a theory which holds that Americans are due no legal protection.' ||
+    '</p><p>' ||
+    'Why? Because a statue might be endangered. For Donald Trump, the concept of human rights has been replaced by the concept of the protection of sacred objects. The president of the United States has declared that the entities that enjoy rights are not people but monuments (all of them, apparently). When federal officials speak now of “violence” or threats to “homeland security”, they do not mean racism or the pandemic but graffiti. Humans figure as potential violators of the moral order created by things. We are to be surveilled because we might be thinking of damaging a statue and arrested or beaten because we are in a place where there is a statue.' ||
+    '</p><p>' ||
+    'This is not just a distraction or an overreaction. It is an anti-democratic form of politics which is characteristic of the new authoritarianism worldwide. In my books I call it the “politics of eternity”: a displacement of the real challenges of the actual world with a myth of a sacred past that must be protected.' ||
+    '</p><p>' ||
+    'We face a racial crisis that threatens African Americans and a pandemic that has taken nearly 150,000 lives. A responsible government would address these problems. The Trump administration is not just distracting us from them by creating lawless mayhem but building a different idea of the task of government – from policy to memory. It is displacing the idea of democratic responsibility with that of the politics of eternity. A politician of eternity seeks to shift the work of government away from the actual problems of actual people and towards the enforcement of national myth.' ||
+    '</p><p>' ||
+    'Although politicians such as Trump like to use the word “history”, the politics of eternity is not at all about history. It is a myth of guilt and innocence. It proposes that, at some mystical moment in the past, our nation (or race) was innocent and perfect, and that we must commune with that image, closing our eyes to the complications of both past and present, and forget entirely about the future. For Trump, statues of Confederate notables, often erected between the two world wars, are the touchstone of our national communion.' ||
+    '</p><p>' ||
+    'Of course, these statues do not embody “history”. The erection and the destruction of monuments are facts in history, not history itself. Like all monuments, Confederate statues represent a moment of social consensus and political power. In this case, the moment was the triumph of white supremacy and the success of racial cleansing in American towns and cities two and three generations after the end of the civil war.' ||
+    '</p><p>' ||
+    'Every monument that has ever gone up will at some point come down. That is what history looks like. During the first world war, Americans insisted on national self-determination in Europe, and at its end the statues of the old emperors came down. After the next world war, American GIs watched in Europe as city squares named after Adolf Hitler were renamed. (The German term for “rapid deployment teams”, by the way, was “taskforces”, or Einsatzgruppen.) Americans cheered when the Lenin monuments came down after the revolutions of 1989. Americans themselves brought down a statue of Saddam Hussein. Should all of this now be restored?' ||
+    '</p><p>' ||
+    'Free people aware of their history can debate and dispute what should go up and what must come down. History is not a fictive permanence where an autocrat tells us that we are right and the others are wrong because we have mystical objects in the sacred locations. History cannot be captured by the dead weight of metal or cement and transformed into an object of worship, the meaning of which is defined by a leader’s whim. That is the politics of eternity, used to make us unfree.' ||
+    '</p><p>' ||
+    'History, by contrast, is about recording and understanding change. It takes for granted that the past structures the present, but also that people can understand those limitations and find ways to transcend them. That is freedom. Democracy requires history, because free people must always be judging our leaders, and ourselves, for our past mistakes. The history of those years between and around the world wars, if we attend to it, tells us things that we need to know.' ||
+    '</p><p>' ||
+    'The black American GIs who returned from the first world war were beaten and humiliated. African Americans were purged from American cities. America after the first world war suffered from a pandemic. We failed, then as now, to establish national public health. Then, as now, a sense of white entitlement got in the way of a system in which everyone would have access to care. We chose “racial hygiene” over public health, and have ever since. Adolf Hitler admired the Jim Crow laws of the interwar years and emulated them. We sent a segregated army to fight the second world war. The men who returned very often found that they could not vote nor enter public places. Then, as now, the separation of African Americans from their basic rights makes us all less free.' ||
+    '</p><p>' ||
+    'American history tells us that freedom is impossible for all when it is denied to the few, and that public health is thwarted by racism. If our democracy is to survive, and if we are to contain a pandemic that has defined this year and a history of racism that goes back 400 years, we will need to reckon with our actual past, insist on everyone’s rights in the present, and plan for a different and better future. The politics of eternity must give way to a politics of responsibility: where we see the past as a source of inspiration, caution, and reflection. Humans have rights because we are capable of this, and democracies will thrive when we do.' ||
+    '</p></section>' ||
+    '<footer>' ||
+    'Timothy Snyder is the Levin professor of history at Yale University. He discusses the politics of eternity in his books <i>On Tyranny</i> and <i>The Road to Unfreedom</i>. His forthcoming book, this September, is entitled <i>Our Malady</i>',
+    '2021-05-31'
+);
+
