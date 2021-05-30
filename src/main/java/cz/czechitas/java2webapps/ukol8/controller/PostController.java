011b5b95ded0007c14723cfaecce3392dd53dad9
TomasProkes <tomas.prokes@centrum.cz>
2021-05-30 13:52:47 +0200

    Add a base template and styles, fix entity and extend controller

--- src/main/java/cz/czechitas/java2webapps/ukol8/controller/PostController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol8/controller/PostController.java
@@ -1,4 +1,28 @@
 package cz.czechitas.java2webapps.ukol8.controller;
 
+import cz.czechitas.java2webapps.ukol8.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Controller for displaying blog posts
+ */
+@Controller
 public class PostController {
+    private final PostService service;
+
+    @Autowired
+    public PostController(PostService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/")
+    public ModelAndView baseList(@PageableDefault(sort = {"published"}) Pageable pageable) {
+        return new ModelAndView("posts")
+                .addObject("posts", service.list(pageable));
+    }
 }
--- src/main/java/cz/czechitas/java2webapps/ukol8/entity/Post.java
+++ src/main/java/cz/czechitas/java2webapps/ukol8/entity/Post.java
@@ -32,4 +32,60 @@ public class Post {
     private String body;
 
     private LocalDate published;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getSlug() {
+        return slug;
+    }
+
+    public void setSlug(String slug) {
+        this.slug = slug;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPerex() {
+        return perex;
+    }
+
+    public void setPerex(String perex) {
+        this.perex = perex;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public LocalDate getPublished() {
+        return published;
+    }
+
+    public void setPublished(LocalDate published) {
+        this.published = published;
+    }
 }
